d8a8f9bf8d7c98570f3d4c57c72f450681686002
Zeki Mirza <zeki.mirza@googlemail.com>
2021-10-10 13:27:32 +0100

    Added home page content and styling and made it the default content for the page

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,11 @@
+import HomeContent from '../pages/homeContent'
+
 const component = (() => {
     const bodyContent = document.createElement('div')
     bodyContent.classList.add('body-content')
+
+    // make the default content
+    bodyContent.appendChild(HomeContent)
 
     const updateContent = content => {
         bodyContent.appendChild(content)
--- src/pages/homeContent.js
+++ src/pages/homeContent.js
@@ -1,9 +1,20 @@
 const component = (() => {
-    let title = document.createElement('p')
-    title.textContent = "Who We Are"
-    title.classList.add('page-title')
+    const homeContent = document.createElement('div')
+    homeContent.classList.add('home-content')
 
-    return title
+
+    const title = document.createElement('p')
+    title.textContent = "Who We Are"
+    title.classList.add('page-title','page-body')
+    homeContent.appendChild(title)
+
+    const body = document.createElement('p')
+    body.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    body.classList.add('page-body')
+    homeContent.appendChild(body)
+
+
+    return homeContent
 })()
 
 export default component
