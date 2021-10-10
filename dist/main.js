634f934708b8837d3d9ee4de967e3180cab5ae77
Zeki Mirza <zeki.mirza@googlemail.com>
2021-10-10 21:07:11 +0100

    added dist folder

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+let test = document.createElement('p')
+test.textContent = "hello"
+document.appendChild(test)
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSwwQiIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJsZXQgdGVzdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ3AnKVxudGVzdC50ZXh0Q29udGVudCA9IFwiaGVsbG9cIlxuZG9jdW1lbnQuYXBwZW5kQ2hpbGQodGVzdCkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
